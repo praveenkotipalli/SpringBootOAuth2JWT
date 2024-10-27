5c88277899b78b9ee08b14a4f4860ef7a5bbc3f3
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2024-10-28 00:18:24 +0530

    first commit

---
+++ praveen/src/main/java/com/learn/praveen/config/RsaKeyProperties.java
@@ -0,0 +1,14 @@
+package com.learn.praveen.config;
+
+
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+
+}
